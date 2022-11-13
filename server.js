1e24e8edbb575a9a134a7541288dcf2e97e097e8
PrajwalTikhe1 <prajwaltikhe10@gmail.com>
2022-11-14 01:09:16 +0530

    make it better

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.use("/api/articles", require("./routes/articles"));
 // Heroku Production
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("frontend/build"));
-  // const path = require("path");
-  // app.get("*", (res, res) => {
-  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  // });
 }
 
 // Endpoint
